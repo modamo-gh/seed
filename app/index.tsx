64af2ffd4e502bc36f99f9d2249c50c3361672f9
Bryan <morganthemosaic@gmail.com>
2025-10-15 09:46:46 -0400

    Add Login UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,26 @@
-import { Stack, Link } from 'expo-router';
+import { useRouter } from 'expo-router';
+import { Image, Linking, Pressable, Text, View } from 'react-native';
 
-import { View } from 'react-native';
+const Login = () => {
+  const router = useRouter();
 
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
-
-export default function Home() {
   return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+    <View className="flex flex-1 items-center justify-center bg-neutral-900">
+      <View className="flex flex-[4] items-center justify-center">
+        <Image className="h-96 w-96" source={require('../assets/playstore.png')} />
+      </View>
+      <View className="flex flex-1 justify-around">
+        <Pressable className="flex h-12 w-64 items-center justify-center rounded-lg bg-green-500 px-2 py-1 active:bg-green-600">
+          <Text className="text-lg font-semibold text-zinc-50">Login with Spotify</Text>
+        </Pressable>
+        <Pressable
+          className="flex h-12 w-64 items-center justify-center"
+          onPress={() => Linking.openURL('https://buymeacoffee.com/modamo')}>
+          <Text className="text-green-500 underline">☕️ Buy me a coffee</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
-
-const styles = {
-  container: 'flex flex-1 bg-white',
 };
+
+export default Login;
--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   printWidth: 100,
-  tabWidth: 2,
-  singleQuote: true,
+  tabWidth: 4,
+  singleQuote: false,
   bracketSameLine: true,
   trailingComma: 'es5',
 
